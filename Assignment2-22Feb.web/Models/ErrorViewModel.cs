fd1e55b2339ae18311eea0ad246dfc54d1e81fb9
Alif <alaminalifaa7@gmail.com>
2020-02-22 12:58:37 +0600

    first commit

---
+++ Assignment2-22Feb.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignment2_22Feb.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
