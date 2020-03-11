a3cd7414b3e6200aaad19774ec38bf8a671bd67b
Alif <alaminalifaa7@gmail.com>
2020-03-11 21:13:32 +0600

    added datatables in DataController

---
+++ Assignment 3-AdminFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignment_3_AdminFrontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
